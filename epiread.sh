76bcd7f21ce5e07e29030929ca708d72a99788dc
caihongx <65621635+caihongx@users.noreply.github.com>
2020-06-08 14:27:57 -0700

    Delete epiread.sh

--- epiread.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-
-biscuit epiread -o s1.chr19.epiread  ../../fastq/hg38.fa ~kims/'Google Drive File Stream'/'My Drive'/chr19bam/chr19_s1.bam
-biscuit epiread -o s2.chr19.epiread  ../../fastq/hg38.fa ~kims/'Google Drive File Stream'/'My Drive'/chr19bam/chr19_s2.bam
-biscuit epiread -o s3.chr19.epiread  ../../fastq/hg38.fa ~kims/'Google Drive File Stream'/'My Drive'/chr19bam/chr19_s3.bam
-biscuit epiread -o s4.chr19.epiread  ../../fastq/hg38.fa ~kims/'Google Drive File Stream'/'My Drive'/chr19bam/chr19_s4.bam
-biscuit epiread -o s5.chr19.epiread  ../../fastq/hg38.fa ~kims/'Google Drive File Stream'/'My Drive'/chr19bam/chr19_s5.bam
-biscuit epiread -o s6.chr19.epiread  ../../fastq/hg38.fa ~kims/'Google Drive File Stream'/'My Drive'/chr19bam/chr19_s6.bam
